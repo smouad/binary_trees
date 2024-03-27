95374056336e352c45ac66850506e5ecaefd95fd
mouad sodor <sodormouad@gmail.com>
2024-03-27 12:32:53 +0000

    edit

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,9 +1,11 @@
 #include "binary_trees.h"
 /**
  * binary_tree_size - function that return the size of a tree
+ * 
  * @tree: tree to check
  * Return: size of tree
  */
+
 size_t binary_tree_size(const binary_tree_t *tree)
 {
 	size_t size = 0, r = 0, l = 0;
--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,24 +1,19 @@
 #include "binary_trees.h"
+
 /**
- * binary_tree_delete - free an entire tree using recursion it free the node
- * when the node left and right are NULL
- * @tree: tree to free
- * Return: Nothing
+ * binary_tree_delete - deletes an entire binary tree
+ *
+ * @tree: pointer to the root node of the tree to delete
+ * If tree is NULL, do nothing
  */
+
 void binary_tree_delete(binary_tree_t *tree)
 {
 	if (tree == NULL)
-	{
 		return;
-	}
-	else
-	{
-		if (tree != NULL)
-		{
-			binary_tree_delete(tree->left);
-			binary_tree_delete(tree->right);
-		}
-		free(tree);
-	}
 
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+
+	free(tree);
 }
